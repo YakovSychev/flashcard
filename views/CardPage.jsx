51cd6af5d9e3fdd40c3a5283278ac806c119645e
Ilya Gamayun <il.gamayun@gmail.com>
2022-11-04 13:32:48 +0300

    Edit

--- views/CardPage.jsx
+++ views/CardPage.jsx
@@ -10,7 +10,6 @@ function CardPage({ quizzes }) {
             <div className="card-body">
               <h5 className="card-title">Special title treatment</h5>
               <input type="text" />
-              <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
               <a href="#" className="btn btn-primary">Go somewhere</a>
             </div>
           </div>
